760aa28894cb4dea024cb1bde4742fb0e3f24bff
Anagha105 <72516635+Anagha105@users.noreply.github.com>
2021-04-27 15:42:15 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "SPONGE";
+const accessCode2 = "PIANO";
+const accessCode3 = "LIBRARY";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("G N E O P S", 100,70)
+    fill("lightblue")
+    text("Riddle: What is full of holes but still holds water?", 100,85)
+
+    fill("white")
+    textSize(15)
+    text("N P A O I", 100,170)
+    fill("pink")
+    text("Riddle: What has many keys but can’t open a single lock?", 100,185)
+
+    fill("white")
+    textSize(15)
+    text("R Y L A B R I", 100,270)
+    fill("lightgreen")
+    text("Riddle: What building has the most stories?", 100,285)
+
+    
+
+}
